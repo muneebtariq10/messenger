74d46fa5e19ea8a427678180b06c673e5546ac62
muneebtariq10 <52176945+muneebtariq10@users.noreply.github.com>
2024-10-14 19:34:55 +0500

    New Dashboard

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,12 +1,12 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard() {
+export default function Home() {
     return (
         <AuthenticatedLayout
             header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Home" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
